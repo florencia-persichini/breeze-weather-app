d9535bcc174bfa5c0b2e52b11c84bd3e44afb736
FlorenciaPersichini <persichini.flor@gmail.com>
2025-02-17 01:50:09 +0100

    Update JS functions for date, time, feels like temperature, condition, humidity and wind speed. Modified strong font color.

--- src/script.js
+++ src/script.js
@@ -1,13 +1,45 @@
 function refreshWeather(response) {
   let cityElement = document.querySelector("#city-name");
-  let temperatureElement = document.querySelector(
-    "#current-temp-value"
-  );
+  let timeElement = document.querySelector("#day-time");
+  let temperatureElement = document.querySelector("#current-temp-value");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+
   let cityName = response.data.city;
   let temperature = Math.round(response.data.temperature.current);
+  let feelsLike = Math.round(response.data.temperature.feels_like);
+  let description = response.data.condition.description;
+  let humidity = response.data.temperature.humidity;
+  let windSpeed = response.data.wind.speed;
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = cityName;
+  timeElement.innerHTML = formattedDate(date);
   temperatureElement.innerHTML = temperature;
+  feelsLikeElement.innerHTML = `${feelsLike}ºC`;
+  descriptionElement.innerHTML = description;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${windSpeed} km/h`;
+}
+
+function formattedDate(date) {
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let weekDay = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${weekDay} ${hours}:${minutes}`;
 }
 
 function searchCityWeather(city) {
@@ -21,10 +53,9 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city-name");
-  let city = searchInput.value;
-  cityElement.innerHTML = city;
+  cityElement.innerHTML = searchInput.value;
 
-  searchCityWeather(city);
+  searchCityWeather(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
